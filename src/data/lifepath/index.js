a50c6f8721d79683b01d5549d940be2031eceafd
88cocapepsi <88cocapepsi@gmail.com>
2026-04-24 16:39:41 +0700

    feat(data): create lifepath index to aggregate all life path number modules

---
+++ src/data/lifepath/index.js
@@ -0,0 +1,25 @@
+import { LIFE_PATH_2 } from "./number_2";
+import { LIFE_PATH_3 } from "./number_3";
+import { LIFE_PATH_4 } from "./number_4";
+import { LIFE_PATH_5 } from "./number_5";
+import { LIFE_PATH_6 } from "./number_6";
+import { LIFE_PATH_7 } from "./number_7";
+import { LIFE_PATH_8 } from "./number_8";
+import { LIFE_PATH_9 } from "./number_9";
+import { LIFE_PATH_10 } from "./number_10";
+import { LIFE_PATH_11 } from "./number_11";
+import { LIFE_PATH_22 } from "./number_22";
+
+export const LIFE_PATH_DATA = {
+  2: LIFE_PATH_2,
+  3: LIFE_PATH_3,
+  4: LIFE_PATH_4,
+  5: LIFE_PATH_5,
+  6: LIFE_PATH_6,
+  7: LIFE_PATH_7,
+  8: LIFE_PATH_8,
+  9: LIFE_PATH_9,
+  10: LIFE_PATH_10,
+  11: LIFE_PATH_11,
+  22: LIFE_PATH_22,
+};
